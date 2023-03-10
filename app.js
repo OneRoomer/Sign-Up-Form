f9dd4a952fb5a838d04eeb87afe1476f60465f24
OneRoomer <rene.elfinger@mailbox.org>
2023-03-10 20:01:13 +0100

    Added full project coded on another PC and refactor Code

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-
+function checkPassword(form) {
+    // get passwords from the field using their name attribute
+    const password = form.password.value;
+    const confirmPassword = form.password_conf.value;
+  
+    // check if both match using if-else condition
+    if (password != confirmPassword) {
+      alert("Error! Password did not match.");
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  const form = document.getElementById("form");
+  
+  document.getElementById("submit").addEventListener("click", function() {
+    checkPassword(form);
+  });
+  
